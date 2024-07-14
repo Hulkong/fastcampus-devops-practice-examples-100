5ca0cfa7fc6b50be4cf86efc818c89c719b46349
Hulkong <kyh@ilevit.com>
2024-07-14 20:34:18 +0900

    chore: update 08-senario of part03

--- 03-강의준비/08-senario/repositories.tf
+++ 03-강의준비/08-senario/repositories.tf
@@ -23,46 +23,13 @@ module "repository-part03-08-senario" {
 #   has_issues             = true
 #   has_projects           = true
 #   has_wiki               = true
+#   private                = false
 #   branch_protections_v3 = [
 #     {
 #       branch                 = "main"
 #       enforce_admins         = false
 #       require_signed_commits = false
 #       required_status_checks = {}
-
-#       required_pull_request_reviews = {
-#         dismiss_stale_reviews           = false
-#         restrict_dismissals             = true
-#         dismissal_teams                 = [module.team_backend_child_01.name]
-#         require_code_owner_reviews      = false
-#         required_approving_review_count = 1
-#       }
-
-#       restrictions = {
-#         teams = [module.team_backend.name]
-#       }
-#     },
-#     {
-#       branch                 = "prod"
-#       enforce_admins         = false
-#       require_signed_commits = false
-
-#       required_pull_request_reviews = {
-#         dismiss_stale_reviews           = false
-#         restrict_dismissals             = true
-#         dismissal_teams                 = [module.team_backend_child_01.name]
-#         require_code_owner_reviews      = false
-#         required_approving_review_count = 1
-#       }
-
-#       restrictions = {
-#         teams = [module.team_backend.name]
-#       }
-#     },
-#     {
-#       branch                 = "staging"
-#       enforce_admins         = false
-#       require_signed_commits = false
 
 #       required_pull_request_reviews = {
 #         dismiss_stale_reviews           = false
